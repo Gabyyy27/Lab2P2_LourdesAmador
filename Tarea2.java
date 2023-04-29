d29152ab35bd6751d743a7cdc317f21414492186
Gabyyy27 <132108243+Gabyyy27@users.noreply.github.com>
2023-04-28 19:31:13 -0600

    Update Tarea2.java

--- Tarea2.java
+++ Tarea2.java
@@ -10,5 +10,17 @@ package Laboratorio2;
  * @author Lourdes
  */
 public class Tarea2 {
-    
+     public static void main(String[]arg){
+        
+        
+      Pinturas p1= new Pinturas("La Gioconda", "Leonardo Da Vinci", 27, 2, 1503, 13, 1, 2018, "Bodega");
+      Pinturas p2= new Pinturas("La noche estrellada", "Vincent Van Gogh", 15, 12, 1889, 22, 7, 2020, "Exposicion");
+      Pinturas p3= new Pinturas("La ultima cena", "Leonardo Da Vinci", 27, 8, 1907, 27, 4, 2023, "Bodega");
+      Pinturas p4= new Pinturas("El Beso", "Gustav Klimt", 27, 02, 1980, 20, 3, 2015, "Exposicion");
+        System.out.println("\n Pintura 1: "+p1);
+        System.out.println("\n Pintura 2: "+p2);
+        System.out.println("\n Pintura 3: "+p3);
+        System.out.println("\n Pintura 4:"+p4);
+    }
+
 }
